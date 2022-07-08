c0d315772acc2d2a83296b7c1e1384ef5253b0fa
iapicca <iapicca@gmail.com>
2022-07-08 18:16:28 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/supertextfield_padding_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.supertextfield_padding_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:super_editor/super_editor.dart';
+
+void main() => runApp(const MaterialApp(home: Application()));
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(context) => Scaffold(
+        body: Center(
+          child: SizedBox.square(
+            dimension: MediaQuery.of(context).size.shortestSide * .5,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+              child: const Center(
+                child: SuperTextField(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+}
